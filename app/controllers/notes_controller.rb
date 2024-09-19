3bdb2a013c99b6e77e3e90f9a7a0739beebe5040
Noah Zitsman <noah@startup.security>
2024-09-18 23:00:52 -0400

    Only cast to explicit note classes.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,6 @@
 class NotesController < ApplicationController
   def show
-    @noteable = params[:type].camelize.constantize.find(params[:id])
+    @noteable = class_for_note(params[:type]).find_by_id(params[:id])
     @notes = @noteable.notes
     @single_note = @noteable.note
     #check if we are showing a single note through the model relationship
@@ -9,7 +9,7 @@ class NotesController < ApplicationController
     render partial: 'notes/modal', locals: { notes: @notes, noteable: @noteable, single_note: @single_note, is_single_note: is_single_note}
   end
   def create
-    @noteable = params[:type].camelize.constantize.find(params[:id])
+    @noteable = class_for_note(params[:type]).find_by_id(params[:id])
     is_single_note = @noteable.class.reflect_on_association(:note).macro == :has_one
     @note = if is_single_note
       @noteable.build_note(content: params[:content])
@@ -20,4 +20,16 @@ class NotesController < ApplicationController
     render partial: 'notes/note', locals: { note: @note }
   end
 
+  private
+
+  def class_for_note(type_name)
+    case type_name
+    when 'GitleaksResult', 'gitleaks_result'
+      GitleaksResult
+    when 'MaskedSecret', 'masked_secret'
+      MaskedSecret
+    else
+      raise "Invalid note type"
+    end
+  end
 end
